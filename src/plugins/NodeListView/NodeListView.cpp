b1ba69129f3a0b50716b04a0a6a76c752a28732f
Dane Gardner <dane.gardner@gmail.com>
2013-06-09 07:33:06 -0700

    Added documentation to NodeListView stub

--- src/plugins/NodeListView/NodeListView.cpp
+++ src/plugins/NodeListView/NodeListView.cpp
@@ -26,6 +26,18 @@
 namespace Plugins {
 namespace NodeListView {
 
+
+/*! \class Plugins::NodeListView::NodeListView
+    \brief
+
+    The node list widget will be a common element to all tools allowing the user to manually select from a list of nodes
+    using the mouse, or using a textual representation of a node list, called "node set" in the rest of this document.
+    This node set will automatically be grouped by job allocation, with job allocations for the current user being
+    pre-selected. Further, a user could manually select nodes 1,2,3,5, and 10 using the mouse, or could type "1-3,5,10"
+    into a text box. This allows both novice and expert users to quickly select node sets for use with a tool.
+ */
+
+
 NodeListView::NodeListView(QWidget *parent) :
     QTreeView(parent)
 {
